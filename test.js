d73c7516ca7ccfe1d689bf5dab49ef8195be5714
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-11-15 15:21:00 +0800

    1

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 const { log } = require("console")
 
 let pwd = '%'
-log(decodeURIComponent((pwd)) == pwd)
+//log(decodeURIComponent((pwd)) == pwd)
+console.log(encodeURIComponent(`qtp@y471%`));
+
+console.log(decodeURIComponent(`qtp%40y471%25`));
